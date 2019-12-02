78a9fb125ad748a54558fcc02fe6d24393a9ba1a
Christian Holme <christian.holme@med.uni-goettingen.de>
2019-12-02 14:49:36 +0100

    easy recon of parts of the data

--- all.sh
+++ all.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 set -e
 
-for i in `seq 1 8` ; do
-	for j in a b c ; do
-
-	prefix=vol_${i}-${j}
-
-	echo $prefix
-
-	./load.sh 3362387 ${prefix} ./data/
-	./grid.sh data/${prefix} ${prefix}_psf2.coo ${prefix}_ksp2.coo > $prefix.log
-	./prep.sh ind/${prefix}.txt ${prefix}_psf2.coo ${prefix}_ksp2.coo ${prefix}_TI.coo ${prefix}_psf.coo ${prefix}_ksp.coo
-	./reco.sh ${prefix}_TI.coo ${prefix}_psf.coo ${prefix}_ksp.coo ${prefix}_reco.coo | tee -a $prefix.log
-	./post.sh ${prefix}_reco.coo ${prefix}_t1map.coo
-
-	done
-done
-
+./single.sh $(seq 1 8)
---
+++ single.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+if [ $# -eq 0 ]; then
+	vols=1
+else
+	vols=$*
+fi
+
+scans=(a b c)
+
+for i in ${vols[@]} ; do
+	for j in ${scans[@]} ; do
+
+		prefix=vol_${i}-${j}
+		./load.sh 3362387 ${prefix} ./data/
+	done
+done
+
+for i in ${vols[@]} ; do
+	for j in ${scans[@]} ; do
+
+		prefix=vol_${i}-${j}
+
+		echo $prefix
+
+		./grid.sh data/${prefix} ${prefix}_psf2.coo ${prefix}_ksp2.coo > $prefix.log
+		./prep.sh ind/${prefix}.txt ${prefix}_psf2.coo ${prefix}_ksp2.coo ${prefix}_TI.coo ${prefix}_psf.coo ${prefix}_ksp.coo
+		./reco.sh ${prefix}_TI.coo ${prefix}_psf.coo ${prefix}_ksp.coo ${prefix}_reco.coo | tee -a $prefix.log
+		./post.sh ${prefix}_reco.coo ${prefix}_t1map.coo
+
+	done
+done
+
